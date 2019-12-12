cc0a9cc87d8dd2257247c4ecc5ed30dcf1e0a7a6
lanhushuang <40765261+lanhushuang@users.noreply.github.com>
2019-12-12 17:54:09 +0800

    Add files via upload

---
+++ Library/ViewLibrary.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+__author__ = 'bowen'
+__date__ = '12/9/2019 9:23 AM'
+
+global DiagConfigNode;
+DiagConfigNode = ["CustomerName", "CustomerID", "ProductName", "SysID", "LOGPATH", "BIOS", "MEMORY", "IDCHK",
+                  "USBPRT", "JUMPER", "SATASPD", "USBSPEED", "PS2", "HWM", "FNTPNL", "PCIESPD", "BUZZER",
+                  "COMPRT", "INTRUDER", "LIGHT"];
